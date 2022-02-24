aa203e0fdb121552b7684e3affe849f1e7b7e5fc
sulavr96 <cyberraid@gmail.com>
2022-02-24 17:57:35 +0200

    First release

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,18 +3,17 @@ import ImageContainer from "../../Components/ImageContainer";
 import { ImagesExtractor } from "../../Helpers/ImagesExtractor";
 
 const Home = () => {
-    // const images = ImagesExtractor(require.context('../../Assets/Images', false, /\.(png|jpe?g|svg)$/));    
+    const images = ImagesExtractor(require.context('../../Assets/Images', false, /\.(png|jpe?g|svg)$/));    
 
     return( 
            <div className="main-container">
-                        {/* <div className="row">
+                        <div className="row">
                         {
                             Object.keys(images).map((key) => (
                                 <ImageContainer key={key} imgSrc={images[key]} />
                             ))
                         }
-                    </div> */}
-                    <p>hello</p>
+                    </div>
                 </div>
        
     )
