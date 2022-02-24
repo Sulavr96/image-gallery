2840f5154b275d4ab425a099ef3212d88b041cd3
sulavr96 <cyberraid@gmail.com>
2022-02-24 17:28:37 +0200

    First release

--- src/Helpers/ImagesExtractor.js
+++ src/Helpers/ImagesExtractor.js
@@ -1,4 +1,5 @@
 export function ImagesExtractor(files) {
+    console.log(files)
     let images = {};
     files.keys().map((item, index) => { images[item.replace('./', '')] = files(item); });
     return images;
